c94ade8be3a786e3d39596398605a12038f69d55
Krzysztof Niepokojczycki <krzysztofn@spacelift.io>
2024-04-15 12:45:08 +0200

    some temps

---
+++ aws-ec2/spacelift/variables.tf
@@ -0,0 +1,14 @@
+// todo
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+variable "suffix" {}
+variable "pulumi_login_url" {}
+variable "pulumi_stack_name" {}
